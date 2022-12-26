f2676f2165a20356e115256bf5dcaf07adfa1910
Maguire Krist <mkrist@dataoceans.com>
2022-12-26 13:00:34 -0500

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/cpp20_env
-# Build directory: /workspaces/cpp20_env/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
